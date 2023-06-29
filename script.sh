92cf5ae6542839beed31f1cb88822556d78162bd
alexenache10 <127024681+alexenache10@users.noreply.github.com>
2023-06-29 15:49:39 +0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Functie care va fi rulata in background, se poate stabili o conexiune 
-background_function() {
-  while true; do
-    sleep 5
-  done
+# Directorul în care sunt stocate aplicațiile
+app_directory="/usr/bin"
+
+# Obține lista de aplicații instalate și selectează aleatoriu una
+select_random_application() {
+  local installed_apps=($(ls $app_directory))
+  local num_apps=${#installed_apps[@]}
+  local random_index=$((RANDOM % num_apps))
+  local random_app=${installed_apps[random_index]}
+  echo "$random_app"
 }
 
-background_function
+# Deschide o aplicație aleatoare
+open_random_application() {
+  local app=$(select_random_application)
+  $app &
+}
+
+# Numărul de aplicații pe care doriți să le deschideți
+num_applications=5
+
+# Deschide aplicațiile aleatoare
+for ((i=1; i<=num_applications; i++)); do
+  open_random_application
+done
+
